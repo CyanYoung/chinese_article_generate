bce4099c151d1c0deb6fd04054aa69f7d4bef57a
yangjc <bupt_yjc@163.com>
2018-09-07 17:45:41 +0800

    add generation constraint

--- generate.py
+++ generate.py
@@ -10,6 +10,7 @@ from util import map_path
 
 
 seq_len = 20
+min_len = 20
 max_len = 100
 
 models = {'rnn_plain': load_model(map_path('rnn_plain')),
@@ -21,7 +22,7 @@ with open(path_word2ind, 'rb') as f:
     word2ind = pk.load(f)
 word_inds = word2ind.word_index
 
-puncs = ['，', '。', '*', '#']
+puncs = ['，', '。']
 punc_inds = [word_inds[punc] for punc in puncs]
 
 ind_words = dict()
@@ -36,12 +37,16 @@ def map_model(name):
         raise KeyError
 
 
-def sample(probs, ind_words):
+def sample(probs, sent_len, word_inds, ind_words, eos):
     max_probs = np.array(sorted(probs, reverse=True)[:10])
     max_probs = max_probs / np.sum(max_probs)
     max_inds = np.argsort(-probs)[:10]
     if max_inds[0] in punc_inds:
         next_ind = max_inds[0]
+    elif sent_len < min_len:
+        next_ind = word_inds[eos]
+        while next_ind == word_inds[eos]:
+            next_ind = choice(max_inds, p=max_probs)
     else:
         next_ind = choice(max_inds, p=max_probs)
     return ind_words[next_ind]
@@ -54,16 +59,13 @@ def predict(text, name, eos):
         sent = bos + sent
     next_word = ''
     while next_word != eos and len(sent) < max_len:
+        sent = sent + next_word
         seq = word2ind.texts_to_sequences([sent])[0]
         align_seq = pad_sequences([seq], maxlen=seq_len)
         model = map_model(name)
         probs = model.predict(align_seq)[0]
-        next_word = sample(probs, ind_words)
-        sent = sent + next_word
-    if sent[-1] == eos:
-        return sent[1:-1]
-    else:
-        return sent[1:]
+        next_word = sample(probs, len(sent), word_inds, ind_words, eos)
+    return sent[1:]
 
 
 if __name__ == '__main__':
