a0d9a75f64c10f327a1165f0a218fe46bcbed602
yangjc <bupt_yjc@163.com>
2018-09-11 09:41:13 +0800

    change fc to da

--- nn_arch.py
+++ nn_arch.py
@@ -2,20 +2,20 @@ from keras.layers import LSTM, Dense, Masking, Dropout
 
 
 def rnn_plain(embed_input, vocab_num):
-    ra = LSTM(200, activation='tanh')
-    fc = Dense(vocab_num, activation='softmax')
+    ra = LSTM(300, activation='tanh')
+    da = Dense(vocab_num, activation='softmax')
     x = Masking()(embed_input)
     x = ra(x)
     x = Dropout(0.2)(x)
-    return fc(x)
+    return da(x)
 
 
 def rnn_stack(embed_input, vocab_num):
-    ra1 = LSTM(200, activation='tanh', return_sequences=True)
-    ra2 = LSTM(200, activation='tanh')
-    fc = Dense(vocab_num, activation='softmax')
+    ra1 = LSTM(300, activation='tanh', return_sequences=True)
+    ra2 = LSTM(300, activation='tanh')
+    da = Dense(vocab_num, activation='softmax')
     x = Masking()(embed_input)
     x = ra1(x)
     x = ra2(x)
     x = Dropout(0.2)(x)
-    return fc(x)
+    return da(x)
