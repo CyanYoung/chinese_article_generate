d9607613eea7011408d724e19a825afb0b980ed7
yangjc <bupt_yjc@163.com>
2018-09-14 16:52:29 +0800

    change nn param

--- nn_arch.py
+++ nn_arch.py
@@ -2,20 +2,20 @@ from keras.layers import LSTM, Dense, Masking, Dropout
 
 
 def rnn_plain(embed_input, vocab_num):
-    ra = LSTM(300, activation='tanh')
+    ra = LSTM(200, activation='tanh')
     da = Dense(vocab_num, activation='softmax')
     x = Masking()(embed_input)
     x = ra(x)
-    x = Dropout(0.2)(x)
+    x = Dropout(0.5)(x)
     return da(x)
 
 
 def rnn_stack(embed_input, vocab_num):
-    ra1 = LSTM(300, activation='tanh', return_sequences=True)
-    ra2 = LSTM(300, activation='tanh')
+    ra1 = LSTM(200, activation='tanh', return_sequences=True)
+    ra2 = LSTM(200, activation='tanh')
     da = Dense(vocab_num, activation='softmax')
     x = Masking()(embed_input)
     x = ra1(x)
     x = ra2(x)
-    x = Dropout(0.2)(x)
+    x = Dropout(0.5)(x)
     return da(x)
