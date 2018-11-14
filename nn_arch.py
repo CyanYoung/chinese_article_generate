e73f2cd1b02dae24fca8f505a9488d1c7d9a91b1
yangjc <bupt_yjc@163.com>
2018-11-14 11:26:34 +0800

    restore time distribute

--- nn_arch.py
+++ nn_arch.py
@@ -1,19 +1,21 @@
-from keras.layers import LSTM, Dense, Dropout
+from keras.layers import LSTM, Dense, Dropout, TimeDistributed
 
 
 def rnn_plain(embed_input, vocab_num):
     ra = LSTM(200, activation='tanh', return_sequences=True)
     da = Dense(vocab_num, activation='softmax')
+    ta = TimeDistributed(da)
     x = ra(embed_input)
     x = Dropout(0.5)(x)
-    return da(x)
+    return ta(x)
 
 
 def rnn_stack(embed_input, vocab_num):
     ra1 = LSTM(200, activation='tanh', return_sequences=True)
     ra2 = LSTM(200, activation='tanh', return_sequences=True)
     da = Dense(vocab_num, activation='softmax')
+    ta = TimeDistributed(da)
     x = ra1(embed_input)
     x = ra2(x)
     x = Dropout(0.5)(x)
-    return da(x)
+    return ta(x)
