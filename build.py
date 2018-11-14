ae934a9c50146a0a7b2cbdd0cf4a24e8ae7b1c21
yangjc <bupt_yjc@163.com>
2018-11-14 21:04:16 +0800

    change to sparse

--- build.py
+++ build.py
@@ -6,7 +6,6 @@ from keras.models import Model
 from keras.layers import Input, Embedding
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
-from keras.utils import to_categorical
 
 from nn_arch import rnn_plain, rnn_stack
 
@@ -42,39 +41,19 @@ def compile(name, embed_mat, seq_len):
     output = func(embed_input, vocab_num)
     model = Model(input, output)
     model.summary()
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
     return model
 
 
-def split(rate, align_seqs, align_inds, vocab_num):
-    bound = int(len(align_seqs) * rate)
-    seq_train, ind_train = align_seqs[:bound], align_inds[:bound]
-    x_dev = np.array(align_seqs[bound:])
-    y_dev = to_categorical(align_inds[bound:], num_classes=vocab_num)
-    return seq_train, ind_train, x_dev, y_dev
-
-
-def get_part(l_rate, step, seq_train, ind_train, vocab_num):
-    l_bound = int(len(seq_train) * l_rate)
-    u_bound = int(len(seq_train) * (l_rate + step))
-    x_train = np.array(seq_train[l_bound:u_bound])
-    y_train = to_categorical(ind_train[l_bound:u_bound], num_classes=vocab_num)
-    return x_train, y_train
-
-
-def fit(name, epoch, embed_mat, align_seqs, align_inds, step):
-    vocab_num, embed_len = embed_mat.shape
+def fit(name, epoch, embed_mat, align_seqs, align_inds):
     seq_len = len(align_seqs[0])
     model = compile(name, embed_mat, seq_len)
     check_point = ModelCheckpoint(map_item(name, paths), monitor='val_loss', verbose=True, save_best_only=True)
-    seq_train, ind_train, x_dev, y_dev = split(0.9, align_seqs, align_inds, vocab_num)
-    for l_rate in np.arange(0, 1, step):
-        x_train, y_train = get_part(l_rate, step, seq_train, ind_train, vocab_num)
-        model.fit(x_train, y_train, batch_size=batch_size, epochs=epoch,
-                  verbose=True, callbacks=[check_point], validation_data=(x_dev, y_dev))
-        del x_train, y_train
+    align_inds = np.expand_dims(align_inds, -1)
+    model.fit(align_seqs, align_inds, batch_size=batch_size, epochs=epoch,
+              verbose=True, callbacks=[check_point], validation_split=0.2)
 
 
 if __name__ == '__main__':
-    fit('rnn_plain', 10, embed_mat, align_seqs, align_inds, step=0.2)
-    fit('rnn_stack', 10, embed_mat, align_seqs, align_inds, step=0.2)
+    fit('rnn_plain', 10, embed_mat, align_seqs, align_inds)
+    fit('rnn_stack', 10, embed_mat, align_seqs, align_inds)
