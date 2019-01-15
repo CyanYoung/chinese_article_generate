11270e92597de07b6fb51f0e0dddec52008d0522
yangjc <bupt_yjc@163.com>
2019-01-15 15:07:30 +0800

    change conv

--- nn_arch.py
+++ nn_arch.py
@@ -5,7 +5,7 @@ win_len = 10
 
 
 def cnn(embed_input, vocab_num):
-    conv = Conv1D(filters=128, kernel_size=win_len, padding='valid', activation=None)
+    conv = Conv1D(filters=128, kernel_size=win_len, padding='valid')
     gate = Conv1D(filters=128, kernel_size=win_len, padding='valid', activation='sigmoid')
     da1 = Dense(200, activation='relu')
     da2 = Dense(vocab_num, activation='softmax')
